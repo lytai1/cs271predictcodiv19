27e592283e00391ebb320cf4665bf52cabc0c3dc
Lok Yi Tai <lokyi.tai@sjsu.edu>
2020-04-13 13:52:27 -0700

    added svm.py

--- code/preprocessing/08_flight_process.py
+++ code/preprocessing/08_flight_process.py
@@ -2,21 +2,32 @@ import pandas as pd
 from datetime import timedelta
 import datetime
 
-path = "../../processed_data/08_flights_from/08_flights_from_"
-start = datetime.date(2020,1,22)
-end = datetime.date(2020,2,4)
-day_count = (end - start).days +1
+# path = "../../processed_data/08_flights_from/08_flights_from_"
+# start = datetime.date(2020,1,22)
+# end = datetime.date(2020,2,4)
+# day_count = (end - start).days +1
 
-df1 = pd.DataFrame()
+# df1 = pd.DataFrame()
 
-for single_date in (start + timedelta(n) for n in range(day_count)):
-    date = single_date.strftime("%Y%m%d")
-    df = pd.read_csv(path + date + ".csv", index_col=0)
-    print(df)
+# for single_date in (start + timedelta(n) for n in range(day_count)):
+#     date = single_date.strftime("%Y%m%d")
+#     df = pd.read_csv(path + date + ".csv", index_col=0)
+#     print(df)
 
-    df1 = pd.concat([df1, df], ignore_index=True, sort=True)
-    print(df1)
+#     df1 = pd.concat([df1, df], ignore_index=True, sort=True)
+#     print(df1)
 
-df1 = df1.apply(pd.Series.value_counts)
-df1.to_csv("../../processed_data/08_flights_no.csv")
+# df1 = df1.apply(pd.Series.value_counts)
+# df1.to_csv("../../processed_data/08_flights_no.csv")
 
+df = pd.read_csv("../../processed_data/08_flights_no.csv", index_col=0)
+
+airports = pd.read_csv("../../processed_data/08_airports.csv", index_col='fs')
+print(airports)
+
+df.rename(columns=airports['city'].to_dict(), inplace=True)
+df['Country'] = df.index.map(airports['countryName'].to_dict())
+df['City'] = df.index.map(airports['city'].to_dict())
+df.dropna(subset=['Country', 'City'], inplace=True)
+
+print(df)
--- code/preprocessing/08_flights.py
+++ code/preprocessing/08_flights.py
@@ -12,24 +12,25 @@ appKey = "bbc12d4c908a7c257bf778d7ef46b890"
 
 callAdr = api + appID + "&appKey=" + appKey
 
-# resp = requests.get(callAdr)
-# if resp.status_code != 200:
-#     # This means something went wrong.
-#     raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-# airportApi = resp.json()
-# airports = json.dumps(airportApi["airports"])
+resp = requests.get(callAdr)
+if resp.status_code != 200:
+    # This means something went wrong.
+    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+airportApi = resp.json()
+# print(airportApi)
+airports = json.dumps(airportApi["airports"])
 
-# # save in pd dataframe and export as CSV
-# df = pd.read_json(airports)
-# print(df)
+# save in pd dataframe and export as CSV
+df = pd.read_json(airports)
+print(df)
 
-# df.to_csv(("../../processed_data/08_airports.csv"))
+df.to_csv(("../../processed_data/08_airports.csv"))
 
-#print to csv file
-# df = pd.read_csv("../../processed_data/08_airports.csv")
-# df = df[['countryName', 'city','name','fs',  'iata', 'icao', 'faa']]
-# print(df)
-# df.to_csv(("../../processed_data/08_airports.csv"))
+# print to csv file
+df = pd.read_csv("../../processed_data/08_airports.csv")
+df = df[['countryName', 'city','name','fs',  'iata', 'icao', 'faa']]
+print(df)
+df.to_csv(("../../processed_data/08_airports_full.csv"))
 
 # remove airports without iata or icao code
 # df = pd.read_csv("../../processed_data/08_airports.csv")
@@ -41,46 +42,46 @@ callAdr = api + appID + "&appKey=" + appKey
 # df.to_csv(("../../processed_data/08_airports.csv"))
 
 #pull data from api to get flight detail
-start = datetime.date(2020,1,25)
-end = datetime.date(2020,2,4)
-day_count = (end - start).days +1
+# start = datetime.date(2020,1,25)
+# end = datetime.date(2020,2,4)
+# day_count = (end - start).days +1
 
-for single_date in (start + timedelta(n) for n in range(day_count)):
-    date = single_date.strftime("%Y/%m/%d")
-    print(date)
-    df = pd.DataFrame()
-    country = pd.read_csv("../../processed_data/08_airports.csv")
-    airport_code = country['iata']
-    for a in airport_code:
-        print(a)
-        departure = []
+# for single_date in (start + timedelta(n) for n in range(day_count)):
+#     date = single_date.strftime("%Y/%m/%d")
+#     print(date)
+#     df = pd.DataFrame()
+#     country = pd.read_csv("../../processed_data/08_airports.csv")
+#     airport_code = country['iata']
+#     for a in airport_code:
+#         print(a)
+#         departure = []
 
-        for hour in range(0, 24, 6):
-            apicall = "https://api.flightstats.com/flex/flightstatus/historical/rest/v3/json/airport/status/"
-            apicall += a
-            apicall += "/arr/"
-            apicall += date
-            apicall += "/" + str(hour) + "?appId="
-            apicall += appID
-            apicall += "&appKey="
-            apicall += appKey +"&utc=false&numHours=6"
+#         for hour in range(0, 24, 6):
+#             apicall = "https://api.flightstats.com/flex/flightstatus/historical/rest/v3/json/airport/status/"
+#             apicall += a
+#             apicall += "/arr/"
+#             apicall += date
+#             apicall += "/" + str(hour) + "?appId="
+#             apicall += appID
+#             apicall += "&appKey="
+#             apicall += appKey +"&utc=false&numHours=6"
 
 
-            resp = requests.get(apicall)
-            if resp.status_code != 200:
-                # This means something went wrong.
-                raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-            airportApi = resp.json()
-            flights = airportApi['flightStatuses']
-            for f in flights:
-                departure.append(f["departureAirportFsCode"])
-                # print(f["departureAirportFsCode"])
-        if len(departure) > 0:
-            print(a)
-            df1 = pd.DataFrame({a: departure})
-            df = pd.concat([df,df1], axis=1)
+#             resp = requests.get(apicall)
+#             if resp.status_code != 200:
+#                 # This means something went wrong.
+#                 raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+#             airportApi = resp.json()
+#             flights = airportApi['flightStatuses']
+#             for f in flights:
+#                 departure.append(f["departureAirportFsCode"])
+#                 # print(f["departureAirportFsCode"])
+#         if len(departure) > 0:
+#             print(a)
+#             df1 = pd.DataFrame({a: departure})
+#             df = pd.concat([df,df1], axis=1)
 
-    print(df)
-    df.to_csv("../../processed_data/08_flights_from_" +single_date.strftime("%Y%m%d")+ ".csv")
+#     print(df)
+#     df.to_csv("../../processed_data/08_flights_from_" +single_date.strftime("%Y%m%d")+ ".csv")
 
 
