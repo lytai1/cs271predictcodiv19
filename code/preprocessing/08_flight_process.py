45cceb70314dde4d061d38dfd688185df83e2ee0
Lok Yi Tai <lokyi.tai@sjsu.edu>
2020-04-14 18:36:26 -0700

    added flights_no_countries.csv withy column national flights and international flights

--- code/preprocessing/08_flight_process.py
+++ code/preprocessing/08_flight_process.py
@@ -2,32 +2,52 @@ import pandas as pd
 from datetime import timedelta
 import datetime
 
-# path = "../../processed_data/08_flights_from/08_flights_from_"
-# start = datetime.date(2020,1,22)
-# end = datetime.date(2020,2,4)
-# day_count = (end - start).days +1
+#combine the 08_flights_from_ csv into one csv
+def combine():
+    path = "../../processed_data/08_flights_from/08_flights_from_"
+    start = datetime.date(2020,1,22)
+    end = datetime.date(2020,2,4)
+    day_count = (end - start).days +1
 
-# df1 = pd.DataFrame()
+    df1 = pd.DataFrame()
 
-# for single_date in (start + timedelta(n) for n in range(day_count)):
-#     date = single_date.strftime("%Y%m%d")
-#     df = pd.read_csv(path + date + ".csv", index_col=0)
-#     print(df)
+    for single_date in (start + timedelta(n) for n in range(day_count)):
+        date = single_date.strftime("%Y%m%d")
+        df = pd.read_csv(path + date + ".csv", index_col=0)
+        print(df)
 
-#     df1 = pd.concat([df1, df], ignore_index=True, sort=True)
-#     print(df1)
+        df1 = pd.concat([df1, df], ignore_index=True, sort=True)
+        print(df1)
 
-# df1 = df1.apply(pd.Series.value_counts)
-# df1.to_csv("../../processed_data/08_flights_no.csv")
+    df1 = df1.apply(pd.Series.value_counts)
+    df1.to_csv("../../processed_data/08_flights_no.csv")
 
-df = pd.read_csv("../../processed_data/08_flights_no.csv", index_col=0)
+def analyse():
+    df = pd.read_csv("../../processed_data/08_flights_no.csv", index_col=0)
 
-airports = pd.read_csv("../../processed_data/08_airports.csv", index_col='fs')
-print(airports)
+    airports = pd.read_csv("../../processed_data/08_airports.csv", index_col='iata')
+    print(airports)
 
-df.rename(columns=airports['city'].to_dict(), inplace=True)
-df['Country'] = df.index.map(airports['countryName'].to_dict())
-df['City'] = df.index.map(airports['city'].to_dict())
-df.dropna(subset=['Country', 'City'], inplace=True)
+    # df.rename(columns=airports['countryName'].to_dict(), inplace=True)
+    df['Country'] = df.index.map(airports['countryName'].to_dict())
+    df.dropna(subset=['Country'], inplace=True)
+    df.set_index(['Country'], inplace=True)
+    df = df.groupby(level="Country").sum()
+    
+    df = df.T
+    df['Country'] = df.index.map(airports['countryName'].to_dict())
+    df['City'] = df.index.map(airports['city'].to_dict())
+    df.set_index(['Country', 'City'], inplace=True)
+    df.sort_index(inplace=True)
 
-print(df)
+    df["National flights"] = df.apply(lambda x: (x[x.name[0]] if x.name[0] in df.columns else 0), axis=1)
+    df["international flights"] = df.sum(axis=1)- 2 * df["National flights"]
+
+    print(df)
+    df.to_csv("../../processed_data/08_flights_no_countries.csv")
+
+
+def main():
+    analyse()
+
+main()
